8591063ade2784b3afc42d902c30678b86309bac
Donald Brooks Arum <brooksd079@gmail.com>
2022-08-17 00:17:14 +0300

    Done.

---
+++ lib/food.rb
@@ -0,0 +1,9 @@
+class Food
+    def recipe
+        puts "this is the recipe for the food"
+    end
+
+    def methodology
+        puts "this are the instriction thats ar used to cook"
+    end
+end
